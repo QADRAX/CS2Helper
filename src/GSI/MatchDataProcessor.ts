41c4810fc966ec09a73fe878774d0ce7b0def7fd
QADRAX <carlos.delacuadra@devo.com>
2025-03-01 17:33:18 +0100

    chore: añadir procesamiento de daño

--- src/GSI/MatchDataProcessor.ts
+++ src/GSI/MatchDataProcessor.ts
@@ -1,15 +1,17 @@
+import { CSState, isValidGameState } from "../types/CSState";
+import { Observer } from "../utils/Observer";
 import { processMatchEvents } from "./processors/matchEvents";
 import { processRoundEvents } from "./processors/roundEvents";
 import { processKillEvents } from "./processors/killEvents";
 import { processDeathEvents } from "./processors/deathEvents";
-import { CSState, isValidGameState } from "../types/CSState";
-import { Observer } from "../utils/Observer";
+import { processDamageReceivedEvents } from "./processors/damageReceivedEvents";
 
 const eventProcessors = [
   processMatchEvents,
   processRoundEvents,
   processKillEvents,
   processDeathEvents,
+  processDamageReceivedEvents,
 ];
 
 /**
