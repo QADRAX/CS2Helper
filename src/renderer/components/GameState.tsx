e7b5bf5da69a841e579f81030f129b9e8a8630b3
QADRAX <carlos.delacuadra@devo.com>
2025-03-04 00:15:02 +0100

    chore: display MatchData in UI

--- src/renderer/components/GameState.tsx
+++ src/renderer/components/GameState.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import GameStateViewer from './GameStateViewer';
-import { useBackendData } from '../hooks/useBackendData';
+import { useAppContext } from '../hooks/useAppContext';
+import MatchDataComponent from './MatchDataComponent';
 
 const GameState: React.FC = () => {
-  const gameState = useBackendData('game-state');
+  const { gameState, matchData } = useAppContext();
 
   if (!gameState) return <div>Waiting for CS:GO...</div>;
 
   return (
     <div>
       <GameStateViewer gameState={gameState} />
+      {matchData && <MatchDataComponent matchData={matchData} />}
     </div>
   );
 };
--- src/renderer/hooks/useAppContext.ts
+++ src/renderer/hooks/useAppContext.ts
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { AppContext } from "../context/Context";
 
-export function useGameState() {
+export function useAppContext() {
   const context = useContext(AppContext);
   if (context === null) {
     throw new Error('useGameState debe ser utilizado dentro de un GameStateProvider');
